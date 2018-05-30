ece5c4d35b6468566b576af807c8a1bc0b5d93a3
sutolll <sssutolll@gmail.com>
2018-05-30 14:06:16 +0100

    Used more descriptive date conversion
    holiday_flg is no longer converted to boolen; got a hunch it's gonna be used as an integer anyway, right?
    
    
    Former-commit-id: 3261109d00b028ba6247dfdc3c74a722e1c162e3

--- forecasting/data.py
+++ forecasting/data.py
@@ -1,20 +1,17 @@
 import pandas as pd
 
 air = {
-    "reserve": pd.read_csv("../data/air/air_reserve.csv", converters={"visit_datetime": pd.to_datetime,
-                                                                      "reserve_datetime": pd.to_datetime}),
+    "reserve": pd.read_csv("../data/air/air_reserve.csv", parse_dates=["visit_datetime", "reserve_datetime"]),
     "store_info": pd.read_csv("../data/air/air_store_info.csv"),
-    "visit_data": pd.read_csv("../data/air/air_visit_data.csv", converters={"visit_date": pd.to_datetime})
+    "visit_data": pd.read_csv("../data/air/air_visit_data.csv", parse_dates=["visit_date"])
 }
 
 hpg = {
-    "reserve": pd.read_csv("../data/hpg/hpg_reserve.csv", converters={"visit_datetime": pd.to_datetime,
-                                                                      "reserve_datetime": pd.to_datetime}),
+    "reserve": pd.read_csv("../data/hpg/hpg_reserve.csv", parse_dates=["visit_datetime", "reserve_datetime"]),
     "store_info": pd.read_csv("../data/hpg/hpg_store_info.csv")
 }
 
-date_info = pd.read_csv("../data/date_info.csv", converters={"calendar_date": pd.to_datetime,
-                                                             "holiday_flg": bool})
+date_info = pd.read_csv("../data/date_info.csv", parse_dates=["calendar_date"])
 store_id_relation = pd.read_csv("../data/store_id_relation.csv")
 
 if __name__ == "__main__":
